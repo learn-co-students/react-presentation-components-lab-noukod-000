075feae3c421692540d09b2b2bcb454580b54cd2
Luke Ghenco <lukeghenco@gmail.com>
2017-06-07 07:18:31 -0400

    Add solution code

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+class DumbComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    this.setState({ mood: newMood });
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
+
+export default DumbComponent;
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,5 @@
-// Code SimplerComponentHere Here
+import React from 'react';
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>;
+
+export default SimplerComponent;
