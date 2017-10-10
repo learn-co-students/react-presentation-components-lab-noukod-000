741fef3be748dcac57ce9114b23380f8befe7aad
Maxwell Benton <maxwellbenton@gmail.com>
2017-10-10 10:39:15 -0400

    Updated package.json to react 16, new enzyme

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-// Code SimplerComponent Here
-=======
 import React from 'react';
 
 const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>;
 
 export default SimplerComponent;
->>>>>>> Add solution code
